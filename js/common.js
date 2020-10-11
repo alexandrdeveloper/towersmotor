c649e59caac25fb682c365c643735654c3094a20
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-10-11 19:50:10 +0300

    slider section

--- js/common.js
+++ js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
-	// Custom JS
+	let appealSlider = $('.appeal-slider');
+
+	appealSlider.slick({
+		slidesToShow: 1,
+		prevArrow: $('.appeal-row .slider-arrow_prev'),
+		nextArrow: $('.appeal-row .slider-arrow_next'),
+		dots: true 
+	});
 
 });
