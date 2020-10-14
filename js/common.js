9da8658b9fc1302127e86784d8f2dee0466d275a
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-10-14 13:46:54 +0300

    footer desktop

---
+++ js/common.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	let appealSlider = $('.appeal-slider');
+	let dillersSlider = $('.dillers-slider');
+	let videoFeedbacks = $('.video-feedbacks__slider');
+	let screenshotFeedback = $('.screenshot-feedback__slider');
+
+	appealSlider.slick({
+		slidesToShow: 1,
+		prevArrow: $('.appeal-row .slider-arrow_prev'),
+		nextArrow: $('.appeal-row .slider-arrow_next'),
+		dots: true 
+	});	
+
+	dillersSlider.slick({
+		slidesToShow: 3,
+		centerMode: true,
+		centerPadding: '10px',
+		prevArrow: $('.dillers .slider-arrow_prev'),
+		nextArrow: $('.dillers .slider-arrow_next'),
+		dots: true,
+	});
+
+	videoFeedbacks.slick({
+		slidesToShow: 2,
+		prevArrow: $('.video-feedbacks .slider-arrow_prev'),
+		nextArrow: $('.video-feedbacks .slider-arrow_next'),
+		dots: true,
+	});
+
+	screenshotFeedback.slick({
+		slidesToShow: 4,
+		prevArrow: $('.screenshot-feedback .slider-arrow_prev'),
+		nextArrow: $('.screenshot-feedback .slider-arrow_next'),
+		dots: true,
+	});
+
+
+
+});
