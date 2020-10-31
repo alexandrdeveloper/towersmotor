f5402341a39bccbe62c3c641fdbc33256c0aa83e
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-10-31 14:39:53 +0200

    adaptive 80%

--- js/common.js
+++ js/common.js
@@ -106,6 +106,31 @@ $(function() {
 		e.preventDefault();
 		$('.submenu-list').slideToggle();
 	});
+
+	$('.gallery-popup').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 	
 
 	
